c38b53c2f71877ba02ec09079cfafbc7f7348a77
Kabir Alkasim <86805577+Croma-jet@users.noreply.github.com>
2023-09-08 22:47:00 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var name=prompt("Please Enter Your Name..")
+    if (name==""){
+        name="";
+      
+    }
+
+alert("Hi "+ name +", May Allh show, all his blessing on you...May you get a lot of Happines, Joy in life... 🙏");
+
+function app(quran,muslim,bukari,muwada,sunan tirmuzi suman,sunan abu dawoda,sunan nisa'i)
+  this.quran = quran;
